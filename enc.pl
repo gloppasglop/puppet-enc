d2c11852c8bf36accf2c2cdbff70e65f7c8f4dea
Glop PasGlop <gloppasglop@gmail.com>
2012-12-08 21:25:33 +0100

    Load inclde recursively

--- enc.pl
+++ enc.pl
@@ -9,7 +9,6 @@ use Data::Dumper;
 my $yamldir="/var/lib/puppet/enc/yaml";
 my $nodename=$ARGV[0];
 
-
 my $node=YAML::LoadFile("$yamldir/$nodename.yaml");
 
 
@@ -45,35 +44,36 @@ foreach ( @searchorder) {
 	push @searchlist, $searchsuffix if defined ( $searchsuffix );
 }
 
-
-foreach ( keys(%{$include} )) {
-	my $included_file_content;
-	next unless $include->{$_} eq 'true' ;
- 	my $base_filename="$yamldir/include/$_.yaml";
-	foreach ( @searchlist ) {
-		my $fn=$base_filename . '.'. $_;
-		printf("looking for %s\n",$fn);
-		if ( -f $fn ) {
-			$included_file_content=YAML::LoadFile($fn);
-			last;
-		}	
-	}
-	# print YAML::Dump($included_file_content);
-	foreach $section ( qw/classes parameters environment/ ) {
-		foreach ( keys(%{$included_file_content->{$section}}) ) {
-			$manifest->{$section}->{$_}=$included_file_content->{$section}->{$_};
+sub load_include
+{
+	my $include=shift;
+	foreach ( keys(%{$include} )) {
+		my $included_file_content;
+		next unless $include->{$_} eq 'true' ;
+ 		my $base_filename="$yamldir/include/$_.yaml";
+		foreach ( @searchlist ) {
+			my $fn=$base_filename . '.'. $_;
+			if ( -f $fn ) {
+				printf("Found %s\n",$fn);
+				$included_file_content=YAML::LoadFile($fn);
+				last;
+			}	
 		}
+		# print YAML::Dump($included_file_content);
+		foreach $section ( qw/classes parameters environment include/ ) {
+			if ( $section eq 'include') {
+				load_include($included_file_content->{$section});
+			} else {
+				foreach ( keys(%{$included_file_content->{$section}}) ) {
+					$manifest->{$section}->{$_}=$included_file_content->{$section}->{$_};
+				}
+			}
+		}
+		
 	}
 }
 
+
+load_include($include);
 print YAML::Dump($manifest);
-exit;
-
-my $common=YAML::LoadFile("/var/lib/puppet/enc/yaml/common.yaml");
-
-foreach $section ( qw/classes parameters environment/ ) {
-	foreach	( keys(%{$common->{$section}}) ) {
-		$manifest->{$section}->{$_}=$common->{$section}->{$_};		
-	}
-}
 
